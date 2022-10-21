a1dc8f5060cb8fc318a81de7d87c3d9ec2544fa0
Walter Perez <walkeriorus472@gmail.com>
2022-10-21 14:33:00 -0300

    Ahora la variable inputs tambien contiene todos los inputs del formulario. Se agrego un objeto que hace un seguimiento de los campos que ya fueron validados. La funcion validarIngresos ahora usa una estructura if-else. La funcion validarCampos ahora actualiza el valor dentro del nuevo objeto camposValidados si un campo paso la validacion o no. Agregada la funcion validarTerminos, que valida el checbox de terminos en funcion de el valor de su atributo checked. Agregado el case 'terminos' en la funcion mostrarMsjError para mostrar el mensaje de error cuando el checkbox terminos no esta seleccionado al presionar el boton de enviar. La funcion flecha que maneja el evento 'submit' del formulario ahora es una funcion nombrada. Se agrego la funcion comprobarCamposValidados, que comprueba el objeto camposValidados y devuelve true solo si todos los campos tienen el valor true.

--- js/form.js
+++ js/form.js
@@ -1,134 +1,169 @@
 let registerForm = document.getElementById('register-form');
-let inputs = document.querySelectorAll('input:not([type=checkbox])')
+let inputs = document.querySelectorAll('#register-form input')
 
 const tests = {
     'user-name': /^[a-zA-Z0-9_-]{6,14}$/,
     'user-pass': /^[a-zA-Z0-9]{8,12}/,
     'user-email': /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9-.]+$/
 }
-
-const validarIngresos = function( event ){
+let camposValidados = {
+    'user-name': false,
+    'user-pass': false,
+    'user-email': false,
+    'terminos': false,
+}
+const validarIngresos = function (event) {
     let name = event.target.name;
     let input = event.target;
-    switch( name ){
-        case 'user-name':
-            validarCampo(input);
-            break
-        case 'user-pass':
-            validarCampo(input);
-            break
-        case 'user-email':
-            validarCampo(input);
-            break
-            
+    if( name != 'terminos'){
+        validarCampo(input);
     }
-
+    else{
+        validarTerminos();
+    }
 }
 let datosUsuario = {
-    'user-name':null,
+    'user-name': null,
     'user-pass': null,
-    'user-email':null,
+    'user-email': null,
     'intereses': []
 }
-const validarCampo = function( input ) {
+const validarCampo = function (input) {
     let name = input.name;
     let valorActual = input.value;
-    if( tests[name].test(valorActual)){
+    if (tests[name].test(valorActual)) {
         //pasa la comprobacion
         input.classList.add('glow-passed');
         input.classList.remove('glow-error');
-        borrarMsjError( name );
+        borrarMsjError(name);
         datosUsuario[name] = valorActual;
+        camposValidados[name] = true;
     }
-    else{
+    else {
         input.classList.add('glow-error');
         input.classList.remove('glow-passed');
-        mostrarMsjError( name );
+        mostrarMsjError(name);
+        camposValidados[name] = false;
     }
 
 }
-const mostrarMsjError = function( inputName ){
+const validarTerminos = function () {
+    let terminos = document.getElementById('terminos');
+    if (terminos.checked) {
+        terminos.classList.remove('glow-error');
+        terminos.classList.add('glow-passed');
+        borrarMsjError(terminos.name)
+        camposValidados['terminos'] = true;
+    }
+    else {
+        terminos.classList.remove('glow-passed');
+        terminos.classList.add('glow-error');
+        mostrarMsjError(terminos.name)
+        camposValidados['terminos'] = false;
+    }
+}
+const mostrarMsjError = function (inputName) {
     let p = document.getElementById(`${inputName}-error`);
-    switch( inputName ){
+    switch (inputName) {
         case 'user-name':
             p.innerText = 'Nombre de usuario no válido';
-            p.classList.remove('noVisible'); 
+            p.classList.remove('noVisible');
             break
         case 'user-pass':
             p.innerText = 'La contraseña debe tener una longitud minima de 8 caracteres y solo puede contener letras y números.';
-            p.classList.remove('noVisible'); 
+            p.classList.remove('noVisible');
             break
         case 'user-email':
             p.innerText = 'Ingrese una dirección de correo válida.';
             p.classList.remove('noVisible');
             break
+        case 'terminos':
+            p.innerText = 'Debe aceptar los terminos y condiciones';
+            p.classList.remove('noVisible');
+            break
 
     }
 }
-const borrarMsjError = function( inputName ){
+const borrarMsjError = function (inputName) {
     let p = document.getElementById(`${inputName}-error`);
     p.innerText = '';
-    p.classList.add('noVisible'); 
+    p.classList.add('noVisible');
 }
-inputs.forEach( input => {
-    input.addEventListener('input', validarIngresos )
-    input.addEventListener('blur',validarIngresos )
+//Añado los event listeners a los inputs del formulario
+inputs.forEach(input => {
+    //Todos los type='text' van a usar los eventos 'input' y 'blur'
+    //El unico type='checkbox' con evento 'change' es el de id terminos
+    if (input.type !== 'checkbox') {
+        input.addEventListener('input', validarIngresos)
+        input.addEventListener('blur', validarIngresos)
     }
+    else {
+        if (input.id === 'terminos') {
+            input.addEventListener('change', validarIngresos)
+        }
+
+    }
+}
 )
-const comprobarDatosUsuario = function(){
+const comprobarDatosUsuario = function () {
     //devuelve false si algun campo no tiene valor
-    for( clave in datosUsuario ){
-        if( datosUsuario[clave] === null){
+    for (clave in datosUsuario) {
+        if (datosUsuario[clave] === null) {
             return false
         }
     }
     return true
 }
-const guardarDatosUsuario = function(){
-    for( prop in datosUsuario ){
-        if(datosUsuario[prop] === null){
+const guardarDatosUsuario = function () {
+    for (prop in datosUsuario) {
+        if (datosUsuario[prop] === null) {
             //si hay un null, ese campo esta vacío
             return false
         }
     }
-    sessionStorage.setItem(datosUsuario['user-name'],JSON.stringify(datosUsuario))
+    sessionStorage.setItem(datosUsuario['user-name'], JSON.stringify(datosUsuario))
     return true
     //si no encuentro ningún campo vacío, guardo el objeto datosUsuario en sessionStorage
 
 }
-
-const comprobarIntereses = function(){
+const comprobarIntereses = function () {
     let checkboxes = document.querySelectorAll('#intereses input[type="checkbox"]');
     let marcados = 0;
-    for( checkbox of checkboxes){
-        if( checkbox.checked){
-            marcados +=1;
-            datosUsuario['intereses'].push( checkbox.value )
+    for (checkbox of checkboxes) {
+        if (checkbox.checked) {
+            marcados += 1;
+            datosUsuario['intereses'].push(checkbox.value)
         }
     }
-    if( marcados === 0){
+    if (marcados === 0) {
         document.getElementById('intereses-error').classList.remove('noVisible');
         return false
     }
-    else{
+    else {
         document.getElementById('intereses-error').classList.add('noVisible');
         return true
     }
 }
-
-registerForm.addEventListener('submit', ( event )=>{
-    let terminos = document.getElementById('terminos').checked;
-    if( comprobarDatosUsuario() && comprobarIntereses() && terminos ){
-        if( sessionStorage.getItem(datosUsuario['user-name']) === null){
+const validarFormulario = function (event) {
+    //Ver que todos los campos esten validados, revisando el objeto camposValidados
+    if ( comprobarDatosUsuario() && comprobarCamposValidados() && comprobarIntereses() ) {
+        if (sessionStorage.getItem(datosUsuario['user-name']) === null) {
             guardarDatosUsuario();
             registerForm.reset();
-        }else{
+        } else {
             event.preventDefault();
         }
-
-    }else{
-            event.preventDefault();
+    } else {
+        event.preventDefault();
     }
 
 }
-)
+const comprobarCamposValidados = function(){
+    for( clave in camposValidados){
+        if( camposValidados[clave] === false){
+            return false
+        }
+    }
+    return true
+}
+registerForm.addEventListener('submit', validarFormulario )
